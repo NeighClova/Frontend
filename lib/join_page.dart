1c93518de3fba0f5c66f1aab5be82ab7e58e56ec
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-05-02 22:50:47 +0900

    feat: Add sign up page (#1)

--- lib/join_page.dart
+++ lib/join_page.dart
@@ -2,35 +2,31 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_neighclova/join_page.dart';
+import 'package:flutter_neighclova/main.dart';
 import 'package:flutter_neighclova/main_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class JoinPage extends StatefulWidget {
+  const JoinPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Login',
-      home: Login(),
-    );
-  }
+  State<JoinPage> createState() => _JoinPageState();
 }
 
-class Login extends StatefulWidget {
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
+class _JoinPageState extends State<JoinPage> {
   TextEditingController controller = TextEditingController();//이메일
   TextEditingController controller2 = TextEditingController();//비밀번호
   bool passwordVisible = false;
+  final _formKey = GlobalKey<FormState>();
+  String userEmail = '';
+  String userPassword = '';
+  
+  void _tryValidation(){
+    final isValid = _formKey.currentState!.validate();
+    if(isValid){
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   void initState() {
     passwordVisible = false;
@@ -55,21 +51,31 @@ class _LoginState extends State<Login> {
           ),
           Padding(padding: EdgeInsets.only(top: 50)),
           Form(
-            child: Theme(
-              data: ThemeData(
-                primaryColor: Colors.grey,
-                inputDecorationTheme: InputDecorationTheme(
-                  labelStyle: TextStyle(color: Colors.teal, fontSize: 15.0))),
-              child: Container(
-                padding: EdgeInsets.all(40.0),
-                //키보드가 올라올 경우 스크롤 되도록
-                child: Builder(builder:(context) {
-                  return Column(
-                    children: [
-                      TextField(
+            key: _formKey,
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              child: Builder(builder: (context) {
+                return Column(
+                  children: [
+                    TextFormField(
+                      validator: (value){
+                        if (value!.isEmpty || !value!.contains('@')){
+                          return '유효한 이메일을 입력해주세요.';
+                        }
+                        else if (value! == 'ex@naver.com') {
+                          return '이미 사용중인 이메일입니다.';
+                        }
+                        else{
+                          return null;
+                        }
+                      },
+                      onSaved: (value){
+                        userEmail = value!;
+                      },
                         controller: controller,
                         decoration: InputDecoration(
-                          hintText: '이메일 입력',
+                          labelText: '이메일',
+                          hintText: 'example@company.com',
                           enabledBorder: OutlineInputBorder(
                             borderSide:BorderSide(
                               color: Colors.grey,
@@ -81,15 +87,50 @@ class _LoginState extends State<Login> {
                               color: Colors.grey,
                               width: 1.0,
                             )
-                          )
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          ),
                         ),
                         keyboardType: TextInputType.emailAddress,
                         style: TextStyle(fontSize: 15),
                       ),
                       Padding(padding: EdgeInsets.only(top: 20)),
-                      TextField(
+                      TextFormField(
+                        validator: (value){
+                          String pwPattern = r'^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,15}$';
+                          RegExp regExp = RegExp(pwPattern);
+                          String pwPattern2 = r'[a-zA-z]';
+                          RegExp regExp2 = RegExp(pwPattern2);
+                          String pwPattern3 = r'[0-9]';
+                          RegExp regExp3 = RegExp(pwPattern3);
+
+                          if (value!.length < 8 || value!.length > 15){
+                            return '8자 이상 15자 이내로 입력하세요.';
+                          }
+                          else if (!regExp2.hasMatch(value!)){
+                            return '영문을 포함해야 합니다.';
+                          }
+                          else if (!regExp3.hasMatch(value!)){
+                            return '숫자를 포함해야 합니다.';
+                          }
+                          return null;
+                      },
+                      onSaved: (value){
+                        userPassword = value!;
+                      },
                         controller: controller2,
                         decoration: InputDecoration(
+                          labelText: '비밀번호',
                           hintText: '비밀번호 입력',
                           enabledBorder: OutlineInputBorder(
                             borderSide:BorderSide(
@@ -98,6 +139,18 @@ class _LoginState extends State<Login> {
                             )
                           ),
                           focusedBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          ),
+                          errorBorder: OutlineInputBorder(
                             borderSide:BorderSide(
                               color: Colors.grey,
                               width: 1.0,
@@ -127,8 +180,10 @@ class _LoginState extends State<Login> {
                         height: 50.0,
                         child: ElevatedButton(
                           onPressed: (){
+                            _tryValidation();
                             ///////////////////////////////////////
-                            if (true/*아이디, 비밀번호 확인*/) {
+                            if (_formKey.currentState!.validate()){
+                              ///////////////////////////////자동으로 로그인
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
@@ -136,11 +191,8 @@ class _LoginState extends State<Login> {
                                   MainPage())
                               );
                             }
-                            else {
-                              showSnackBar(context, Text('이메일이나 비밀번호가 옳지 않습니다.'));
-                            }
                           },
-                          child: Text('로그인', style: TextStyle(color:Colors.white)),
+                          child: Text('회원가입', style: TextStyle(color:Colors.white)),
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Color(0xff03C75A),
                             shape: RoundedRectangleBorder(
@@ -150,42 +202,35 @@ class _LoginState extends State<Login> {
                           ),
                         )
                       ),
-                      Padding(padding: EdgeInsets.only(top: 20)),
+                      SizedBox(height: 10.0,),
                       Row(
                         mainAxisSize: MainAxisSize.max,
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                        TextButton(
-                        onPressed: (){
-                          Navigator.push(
+                          Text('이미 계정이 있으신가요?',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: (){
+                              Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                   builder: (BuildContext context) =>
-                                  PasswordPage())
+                                  Login())
                               );
-                        },
-                        child: Text('비밀번호 찾기', style: TextStyle(color:Colors.black)),
+                            },
+                            child: Text('로그인',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.black,
+                                fontSize: 12,
+                              ),),
+                          )
+                        ],
                       ),
-                      Padding(padding: EdgeInsets.fromLTRB(0, 0, 30, 0)),
-                      Container(
-                        width: 1.0,
-                        height: 15,
-                        color: Colors.grey,
-                        alignment: Alignment.center,
-                      ),
-                      Padding(padding: EdgeInsets.fromLTRB(55, 0, 0, 0)),
-                      TextButton(
-                        onPressed: (){
-                          Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (BuildContext context) =>
-                                  JoinPage())
-                              );
-                        },
-                        child: Text('회원가입', style: TextStyle(color:Colors.black)),
-                      ),
-                      ],),
                       Padding(padding: EdgeInsets.only(top: 100.0)),
                       Row(
                         mainAxisSize: MainAxisSize.max,
@@ -222,16 +267,12 @@ class _LoginState extends State<Login> {
                           width: 50.0,
                         ),
                       ),
-                    ],
-                  );
-                }
-                
-                ),
-              )
-              ),
-
-          )
-          ],
+                  ],
+                );
+              },)
+            ),
+            )
+            ]
           )
         )
       )
@@ -246,24 +287,4 @@ void showSnackBar(BuildContext context, Text text) {
   );
 
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
-
-
-
-//비밀번호 찾기 페이지
-class PasswordPage extends StatelessWidget {
-  const PasswordPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: Text('비밀번호찾기'),
-          )
-      ],),
-        
-    );
-  }
 }
