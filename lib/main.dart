7309bd753528238ba85e1b4a348fddffd37987cc
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-05-02 18:03:17 +0900

    feat: Add login page (#1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_neighclova/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +11,273 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Login',
+      home: Login(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Login extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Login> createState() => _LoginState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _LoginState extends State<Login> {
+  TextEditingController controller = TextEditingController();//이메일
+  TextEditingController controller2 = TextEditingController();//비밀번호
+  bool passwordVisible = false;
+  @override
+  void initState() {
+    passwordVisible = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+          Padding(padding: EdgeInsets.only(top: 180)),
+          Center(
+            child: Image(
+              image: AssetImage('assets/logo.png'),
+              width: 250.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 50)),
+          Form(
+            child: Theme(
+              data: ThemeData(
+                primaryColor: Colors.grey,
+                inputDecorationTheme: InputDecorationTheme(
+                  labelStyle: TextStyle(color: Colors.teal, fontSize: 15.0))),
+              child: Container(
+                padding: EdgeInsets.all(40.0),
+                //키보드가 올라올 경우 스크롤 되도록
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: controller,
+                        decoration: InputDecoration(
+                          hintText: '이메일 입력',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          )
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 20)),
+                      TextField(
+                        controller: controller2,
+                        decoration: InputDecoration(
+                          hintText: '비밀번호 입력',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            )
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              passwordVisible ? Icons.visibility : Icons.visibility_off,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              passwordVisible = !passwordVisible;
+                              print(passwordVisible);
+                              setState(() {
+                                
+                              });
+                            },
+                          )
+                        ),
+                        keyboardType: TextInputType.text,
+                        style: TextStyle(fontSize: 15),
+                        obscureText: passwordVisible ? false : true,
+                      ),
+                      SizedBox(height: 30.0,),
+                      ButtonTheme(
+                        //minWidth: 1000.0,
+                        height: 50.0,
+                        child: ElevatedButton(
+                          onPressed: (){
+                            ///////////////////////////////////////
+                            if (true/*아이디, 비밀번호 확인*/) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                  MainPage())
+                              );
+                            }
+                            else {
+                              showSnackBar(context, Text('이메일이나 비밀번호가 옳지 않습니다.'));
+                            }
+                          },
+                          child: Text('로그인', style: TextStyle(color:Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xff03C75A),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                            ),
+                            minimumSize: Size.fromHeight(50),
+                          ),
+                        )
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 20)),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                        TextButton(
+                        onPressed: (){
+                          Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                  PasswordPage())
+                              );
+                        },
+                        child: Text('비밀번호 찾기', style: TextStyle(color:Colors.black)),
+                      ),
+                      Padding(padding: EdgeInsets.fromLTRB(0, 0, 30, 0)),
+                      Container(
+                        width: 1.0,
+                        height: 15,
+                        color: Colors.grey,
+                        alignment: Alignment.center,
+                      ),
+                      Padding(padding: EdgeInsets.fromLTRB(55, 0, 0, 0)),
+                      TextButton(
+                        onPressed: (){
+                          Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                  JoinPage())
+                              );
+                        },
+                        child: Text('회원가입', style: TextStyle(color:Colors.black)),
+                      ),
+                      ],),
+                      Padding(padding: EdgeInsets.only(top: 100.0)),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Container(
+                            constraints: BoxConstraints(maxWidth: 1000.0,),
+                            height: 1,
+                            width: 1000.0,
+                            color:Colors.grey,
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        Text('네이버로 간편하게 로그인하기', style: TextStyle(color: Colors.black)),
+                        SizedBox(width: 10.0),
+                          Expanded(
+                            child: Container(
+                            constraints: BoxConstraints(maxWidth: 1000.0,),
+                            height: 1,
+                            width: 1000.0,
+                            color:Colors.grey,
+                            ),
+                          )
+                      ],
+                      ),
+                      SizedBox(height: 10.0),
+                      InkWell(
+                        onTap:() {
+                          print("네이버 로그인");
+                        },
+                        child: Image(
+                          image: AssetImage('assets/naver_logo.png'),
+                          width: 50.0,
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+              )
+              ),
+
+          )
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )
+        )
+      )
+    );
+  }
+}
+
+void showSnackBar(BuildContext context, Text text) {
+  final snackBar = SnackBar(
+    content: text,
+    backgroundColor: Color(0xff03C75A),
+  );
+
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+
+
+//비밀번호 찾기 페이지
+class PasswordPage extends StatelessWidget {
+  const PasswordPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text('비밀번호찾기'),
+          )
+      ],),
+        
+    );
+  }
+}
+
+//회원가입 페이지
+class JoinPage extends StatelessWidget {
+  const JoinPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text('회원가입'),
+          )
+      ],),
     );
   }
 }
