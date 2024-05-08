acfafe2a5afe512cefc2b8262148fee2dbf58909
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-05-09 00:01:08 +0900

    feat: Add app bar

--- lib/news.dart
+++ lib/news.dart
@@ -1,14 +1,39 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_neighclova/shared/bottom.dart';
 
-class NewsPage extends StatelessWidget {
+class NewsPage extends StatefulWidget {
   const NewsPage({Key? key}) : super(key: key);
 
+  @override
+  State<NewsPage> createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: Bottom(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        shape: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          )
+        ),
+        title: Text('소식 생성',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 20,
+          )
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
     );
   }
 }
