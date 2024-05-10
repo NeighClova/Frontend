9c02b4266161f49e57a361aa1dddde26bd4cb8a3
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-05-10 23:27:50 +0900

    feat: Add choice chips (#2)

--- lib/generate_news.dart
+++ lib/generate_news.dart
@@ -9,6 +9,24 @@ class GenerateNews extends StatefulWidget {
 }
 
 class _GenerateNewsState extends State<GenerateNews> {
+  List<bool> keyword_isSelected = [false, false, false, false, false]; 
+  List<bool> type_isSelected = [false, false, false, false, false];
+  final List<Map<String, dynamic>> keyword = [
+    {'keyword': '알림', 'isSelected': false},
+    {'keyword': '임시 휴무', 'isSelected': false},
+    {'keyword': 'EVENT', 'isSelected': false},
+    {'keyword': 'SALE', 'isSelected': false},
+    {'keyword': 'NEW', 'isSelected': false},
+  ];
+  final List<Map<String, dynamic>> type = [
+    {'type': '가격 인상 안내', 'isSelected': false},
+    {'type': '매장 이용 안내', 'isSelected': false},
+    {'type': '메뉴 홍보', 'isSelected': false},
+    {'type': '깜짝 이벤트', 'isSelected': false},
+  ];
+  DateTime initialDay = DateTime.now();
+  var daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+
   @override
 	Widget build(BuildContext context) {
 		return Scaffold(
@@ -40,6 +58,8 @@ class _GenerateNewsState extends State<GenerateNews> {
               Padding(
                 padding: EdgeInsets.all(20),
                 child: Column(
+                  /////////////////////////////////
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -86,7 +106,109 @@ class _GenerateNewsState extends State<GenerateNews> {
                         ),
                       ],
                     ),
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(keyword.length, (index) {
+                        return buildKeyword(index);
+                      }),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20)),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('소식 유형',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text('구체적으로 어떤 종류의 소식을 작성하시겠어요?',
+                            style: TextStyle(
+                              color:Colors.grey,
+                              fontSize: 15,
+                            )
+                          ),
+                        ]
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(type.length, (index) {
+                        return buildType(index);
+                      }),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    TextFormField(
+                      minLines: 2,
+                      maxLines: 5,
+                      style: TextStyle(fontSize: 12),
+                      decoration: InputDecoration(
+                        hintText: '추가로 원하는 소식의 유형을 작성해주세요.',
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:BorderSide(
+                            color: Colors.grey,
+                            width: 1.0,
+                          )
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:BorderSide(
+                            color: Colors.grey,
+                            width: 1.0,
+                          )
+                        )
+                      ),
+                    ),
 
+                    Padding(padding: EdgeInsets.only(top: 20)),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('기간',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text('특별 이벤트나 세일의 유효 기간 등을 작성해주세요.',
+                            style: TextStyle(
+                              color:Colors.grey,
+                              fontSize: 15,
+                            )
+                          ),
+                        ]
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () async {
+                            final DateTime? dateTime = await showDatePicker(
+                              context: context,
+                              initialDate: initialDay,
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(3000)
+                            );
+                            if (dateTime != null) {
+                              setState(() {
+                                initialDay = dateTime;
+                              });
+                            }
+                          },
+                          child: Text('${initialDay.month}월 ${initialDay.day}일')
+                        )
+                      ],
+                    )
                   ],
                 ),
               ),
@@ -96,4 +218,98 @@ class _GenerateNewsState extends State<GenerateNews> {
       )
 		);
 	}
+
+  int selectedKeywordIndex = -1;
+  Widget buildKeyword(index) {
+    return Padding(
+      padding: EdgeInsets.only(right: 4),
+      child: ChoiceChip(
+        labelPadding: EdgeInsets.all(0),
+        label: SizedBox(
+          width: 50,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              keyword[index]['keyword'],
+              style: TextStyle(
+              color: keyword[index]['isSelected'] ? Colors.white : Colors.black,
+              fontSize: 11,
+              )
+            ),
+          ),
+        ),
+        selected: selectedKeywordIndex == index,
+        selectedColor: Color(0xff03AA5A),
+        backgroundColor: Color(0xffF2F2F2),
+        showCheckmark: false,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+          side: BorderSide(color: Colors.transparent),
+        ),
+        onSelected: (value) {
+          setState(() {
+            if (selectedKeywordIndex == index) {
+              selectedKeywordIndex = -1;
+            }
+            else {
+              selectedKeywordIndex = index;
+            }
+            for (int i = 0; i < keyword.length; i++) {
+              if (i != index) {
+                keyword[i]['isSelected'] = false;
+              }
+            }
+          });
+        },
+        elevation: 10,
+      ),
+    );
+  }
+
+  int selectedTypeIndex = -1;
+  Widget buildType(index) {
+    return Padding(
+      padding: EdgeInsets.only(right: 4),
+      child: ChoiceChip(
+        labelPadding: EdgeInsets.all(0),
+        label: SizedBox(
+          width: 70,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              type[index]['type'],
+              style: TextStyle(
+              color: type[index]['isSelected'] ? Colors.white : Colors.black,
+              fontSize: 10,
+              )
+            ),
+          ),
+        ),
+        selected: selectedTypeIndex == index,
+        selectedColor: Color(0xff03AA5A),
+        backgroundColor: Color(0xffF2F2F2),
+        showCheckmark: false,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+          side: BorderSide(color: Colors.transparent),
+        ),
+        onSelected: (value) {
+          setState(() {
+            if (selectedTypeIndex == index) {
+              selectedTypeIndex = -1;
+            }
+            else {
+              selectedTypeIndex = index;
+            }
+            for (int i = 0; i < type.length; i++) {
+              if (i != index) {
+                type[i]['isSelected'] = false;
+              }
+            }
+          });
+        },
+        elevation: 10,
+      ),
+    );
+  }
 }
