44f0e0542d4b79d9b7fbd8100caaaa9b0631db36
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-05-10 23:31:34 +0900

    chore: Modify pubspec.yaml (#2)

--- lib/generate_news.dart
+++ lib/generate_news.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 class GenerateNews extends StatefulWidget {
 	const GenerateNews({Key? key}) : super(key: key);
